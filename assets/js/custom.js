83ebb14597603755431708d1463c6c7cf45fe545
NamaiBest <namaimailm@gmail.com>
2025-12-02 17:29:41 +0530

    Minor bug fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,18 +7,13 @@ Note: Please do not remove the footer backlink (webthemez.com)--(if you want to 
 Licence: Creative Commons Attribution 3.0** - http://creativecommons.org/licenses/by/3.0/
 */
 jQuery(document).ready(function ($) {
-    //da slider
-    $('#da-slider').cslider({
-        autoplay: true,
-        bgincrement: 0
-    });
-    //Set the carousel options
+    // da slider - removed as cslider plugin is not loaded
+    // Set the carousel options
     $('#quote-carousel').carousel({
         pause: true,
         interval: 4000,
     });
-    // fancybox
-    $(".fancybox").fancybox();
+    // fancybox - removed as fancybox plugin is not loaded
     //isotope
     if ($('.isotopeWrapper').length) {
         var $container = $('.isotopeWrapper');
